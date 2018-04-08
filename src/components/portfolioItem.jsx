e0a7fd449987b7f9197e1696b18a4c2ab1fde455
Sasha <sashikers@gmail.com>
2018-04-08 16:28:54 -0700

    portfolio page style changes

--- src/components/portfolioItem.jsx
+++ src/components/portfolioItem.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 
 const PortfolioItem = props => (
-    <div className="card">
+    <div className="card horizontal">
         <div className="card-image">
             <img className="portfolioScreenshot" src={props.screenshot} />
+        </div>
+        <div className="card-stacked">
+            <div className="card-content">
+                <span className="card-title">{props.name}</span>
+                <p className="projectDescription">{props.description}</p>
+            </div>
+            <div className="card-action">
+                <a className="cardLink" href={props.link}>Link</a>
+                <a className="cardLink" href={props.github}>Github</a>
             
-        </div>
-        <div className="card-content">
-            <span className="card-title">{props.name}</span>
-            <p className="projectDescription">{props.description}</p>
-            
-        </div>
-        <div className="card-action">
-            <a className="cardLink" href={props.link}>Link</a>
-            <a className="cardLink" href={props.github}>Github</a>
-        
-        </div>
+            </div>
 
+        </div>
+        
 
         <div className=""></div>
         <div className=""></div>

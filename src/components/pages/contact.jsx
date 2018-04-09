b8076601b80e93e84238f1ae3491bad83a381984
Sasha <sashikers@gmail.com>
2018-04-08 17:53:30 -0700

    basic contact page setup

--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -3,7 +3,32 @@ import React from 'react';
 const Contact = () => (
     <div className="row">
         <div className="col s12 m8">
-            <h5>Contact me</h5>
+            
+            <div className="row">
+                <h5>Contact me</h5>
+                <form className="col s12">
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input id="commentName" type="text" className="validate" />
+                            <label htmlFor="commentName">Name: </label>
+                        </div>
+                        <div className="input-field col s6">
+                            <input id="commentEmail" type="email" className="validate" />
+                            <label htmlFor="commentEmail">Email: </label>
+                        </div>
+
+
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <textarea id="commentTextArea" className="materialize-textarea"></textarea>
+                            <label htmlFor="commentTextArea">Comment: </label>
+                        </div>
+                    </div>
+                    
+                    <a className="waves-effect waves-light btn" id="submitButton">Submit</a>
+                </form>
+            </div>
         </div>
         <div className="col s12 m4">
             <h5>Links</h5>
